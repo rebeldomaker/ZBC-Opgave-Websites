14d5dd23871d5d3c43efe7f0fe93bfee5c8d9853
Reb <rebeldomaker@icloud.com>
2026-09-08 16:56:12 +0200

    //

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+// todo show top 10 from the latest F1 Race participates in a race.
+// todo The race should illustrate who is Winning (JavaScript and Bootstrap) to get its cars to to move.
+// todo It must be clear which car won the race of the year.
+// todo every car must have team name, points and ranking.
+// You decide for yourself where on the page
+// The information is provided that it is not misleading."
+
+// console.log('Happy developing ✨')
+
