4f0dd12008297b51fffd229de0b47a0099f2dc3c
Reb <rebeldomaker@icloud.com>
2026-09-09 11:50:54 +0200

    added (not yet visible) carousel/slideshow in js (inside the html file)

---
+++ GroupProjectWebsite/main.js
@@ -0,0 +1,17 @@
+// carousel aka infinite Slideshow in Belt Loop form
+const slideshow = document.createElement("div");
+slideshow.className = "slideshow";
+const mover = document.createElement("div");
+mover.className = "mover-1";
+
+for (let i = 0; i < 12; i++) {
+    const slideImg = document.createElement("img");
+    slideImg.src = "/about/image_2023-07-30_10-08-06.png";
+    slideImg.alt = "3d text";
+    mover.appendChild(slideImg);
+}
+slideshow.appendChild(mover);
+contentDiv.appendChild(slideshow);
+
+appendImg("/about/purpleblood.gif", "divider", contentDiv);
+appendImg("/about/beanie3.gif", "decoration", contentDiv);
